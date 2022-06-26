a1dc3edb190c91d889630bce243772ccae3f2767
varunbgit <vbansalevents01@gmail.com>
2022-06-26 18:00:52 +0530

    Find the duplicate element in the array

--- Sorting Assignments/src/com/varun/FindDuplicateElement.java
+++ Sorting Assignments/src/com/varun/FindDuplicateElement.java
@@ -1,2 +1,30 @@
-package com.varun;public class FindDuplicateElement {
+package com.varun;
+
+//287. Find the Duplicate Number
+
+public class FindDuplicateElement {
+    public static void main(String[] args) {
+        int []arr = {1,3,4,2,2};
+        System.out.println(findDuplicate(arr));
+    }
+
+    public static int findDuplicate(int[] nums) {
+        int index = 0;
+        while(index< nums.length){
+            if(nums[index] != index+1 && nums[index]!= nums[nums[index]-1]){
+                swap(nums,index,nums[index]-1);
+            }else{
+                index++;
+            }
+        }
+
+        return nums[nums.length-1];
+    }
+
+    public static void swap(int [] arr,int index1,int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp ;
+    }
+
 }
